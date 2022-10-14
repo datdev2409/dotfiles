acb43f576be0f2ad5a53e64da9264ea6324a3308
congdat <datdev2409@gmail.com>
2022-10-14 19:51:08 +0700

    update bash script

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
+ssh-keygen -t ed25519 -C "datdev2409@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub
+
 sudo pacman -Syu
 sudo pacman -S kitty
 
 ln -s ~/dotfiles/qtile/ ~/.config/
 ln -s ~/dotfiles/nvim/ ~/.config/
 ln -s ~/dotfiles/kitty/ ~/.config/
+ln -s ~/dotfiles/.gitconfig ~/
 
 # Packer nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
